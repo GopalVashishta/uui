676d52c8f38348dff75ef55f17a17e300c38ab86
GopalVashishta <150985276+GopalVashishta@users.noreply.github.com>
2025-03-13 22:35:24 +0530

    All the remaining files that were forgotten
    i made the project but forgot to push it to the github

---
+++ assets/js/nex.js
@@ -0,0 +1,5 @@
+document.getElementById("nexus").textContent='';
+document.getElementById("h2sechead").textContent="what we say";
+document.getElementById("suskey").textContent='Sustainability is the key to a greater future.';
+document.getElementById("me").textContent="gopalvashishta@gmail.com";
+document.getElementById("frp").textContent="Food Resourse Production";
